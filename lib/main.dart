49c1e86985077f27816965979c0f734284f9abd2
MickeyNice325 <81234662+MickeyNice325@users.noreply.github.com>
2025-01-13 09:33:48 +0700

    13/1/2568

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+
 import 'package:flutter/material.dart';
 // import 'package:flutter_application_1/screen/home.dart';
 // import 'package:flutter_application_1/tabbar.dart';
 import 'package:flutter_application_1/googlenavbar.dart';
+import 'package:lottie/lottie.dart';
+
 
 class FoodMenu {
   final String name;
@@ -26,7 +29,70 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'NotoSansThai',
       ),
-      home: Example(),
+      home: lottiescreen(),  // Starting screen
+    );
+  }
+}
+
+class lottiescreen extends StatefulWidget {
+  const lottiescreen({super.key});
+
+  @override
+  State<lottiescreen> createState() => _lottiescreenState();
+}
+
+class _lottiescreenState extends State<lottiescreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextPage();
+  }
+
+  void _navigateToNextPage() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => Example(), // Replace with your actual Example widget
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = 0.0; // Start from 0 for a fade-in effect
+            const end = 1.0;   // End at 1 for full opacity
+            const curve = Curves.easeInOut;
+            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var opacityAnimation = tween.animate(animation);
+            var fadeChild = FadeTransition(
+              opacity: opacityAnimation,
+              child: child,
+            );
+            return fadeChild;
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('asset/images/Aniki_Hamster.json'), // Ensure path is correct
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Welcome to my ball',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
